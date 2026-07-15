c068089eb216fcfa99c0e29b438cf451c2be6150
NetZuLink <93989322+dispatchgrid@users.noreply.github.com>
2026-07-15 17:13:55 +0530

    Minor

--- inDev.bat
+++ inDev.bat
@@ -9,11 +9,8 @@ for /f %%i in ('git rev-parse HEAD') do set LOCAL=%%i
 for /f %%i in ('git rev-parse origin/main') do set REMOTE=%%i
 
 if "%LOCAL%"=="%REMOTE%" (
-    echo No new commits. Continuing...
-    REM Put your normal workflow here
+    run.bat
 ) else (
-    echo New commits detected! Doing something else...
-    REM Put your update logic here (e.g., git pull, notify, etc.)
+    update.bat
 )
 
-pause
