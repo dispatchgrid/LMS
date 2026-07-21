26141eba3d58b13cfd36216293f44a148d68f2c5
dispatchgrid <93989322+dispatchgrid@users.noreply.github.com>
2026-07-21 22:23:13 +0530

    Always Update

--- run.bat
+++ run.bat
@@ -1,14 +1,6 @@
 @echo off
-cd /d %~dp0
-
 git fetch origin
-
-for /f %%i in ('git rev-parse HEAD') do set LOCAL=%%i
-for /f %%i in ('git rev-parse origin/main') do set REMOTE=%%i
-
-if "%LOCAL%"=="%REMOTE%" (
-    start.bat
-) else (
-    update.bat
-)
-
+git reset --hard origin/main
+call npm install
+timeout /t 5 /nobreak >nul
+call start.bat
